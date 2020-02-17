befbb48b4f69a7f3fe5fc19c6e05d4704a276b96
Nolat <NolatYT@gmail.com>
2020-02-17 11:28:12 +0100

    :card_file_box: (AT-9): Add Faction entity

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,1 +1,3 @@
 export { default as User } from "./User/User";
+
+export { default as Faction } from "./Faction/Faction";
--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,1 +1,3 @@
 export { default as UserResolver } from "./User/UserResolver";
+
+export { default as FactionResolver } from "./Faction/FactionResolver";
