8951a1597358ea73fa888dfab89fd7216a386da9
Nolat <NolatYT@gmail.com>
2020-03-02 11:07:04 +0100

    :pencil: Add regles-conquetes

--- src/utils/discord/helpers/sendReglementMessage.ts
+++ src/utils/discord/helpers/sendReglementMessage.ts
@@ -5,10 +5,11 @@ import sendOrUpdateServerMessage from "./sendOrUpdateServerMessage";
 
 const sendReglementMessage = async (server: Guild) => {
   sendOrUpdateServerMessage(server, "#reglement");
+  sendOrUpdateServerMessage(server, "#regles-conquetes");
   sendOrUpdateServerMessage(server, "#overwatch-3v3");
   sendOrUpdateServerMessage(server, "#overwatch-6v6");
   sendOrUpdateServerMessage(server, "#counter-strike-2v2");
-  sendOrUpdateServerMessage(server, "#counter-strike-6v6");
+  sendOrUpdateServerMessage(server, "#counter-strike-5v5");
   sendOrUpdateServerMessage(server, "#league-of-legends-5v5");
   sendOrUpdateServerMessage(server, "#super-smash-bros-1v1");
   sendOrUpdateServerMessage(server, "#super-smash-bros-2v2");
