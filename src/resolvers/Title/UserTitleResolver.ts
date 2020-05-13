29af5fa6b0fabaae075352166f8b4f3af8025588
Alex_Amemoutou <a.amemoutou-external@overkiz.com>
2020-05-13 15:23:02 +0200

    :sparkles: Unset active title user #AT-45

--- src/resolvers/Title/UserTitleResolver.ts
+++ src/resolvers/Title/UserTitleResolver.ts
@@ -135,4 +135,34 @@ export default class UserTitleResolver {
     userTitle.title = title;
     return userTitle.save();
   }
+
+  @Authorized()
+  @Mutation(() => UserTitle)
+  async unsetUserTitle(@Arg("id") id: string) {
+    const user: User | undefined = await User.findOne({ where: { id } });
+
+    if (!user)
+      throw new ApolloError(
+        `Cannot find user with id : ${id}`,
+        "USER_DOESNT_EXIST"
+      );
+
+    const userTitle: UserTitle | undefined = await UserTitle.findOne({
+      relations: ["user", "title"],
+      where: {
+        user,
+        isEnabled: true
+      }
+    });
+
+    if (!userTitle)
+      throw new ApolloError(
+        `The user ${id} doesn't have any title set`,
+        "USER_DOESNT_HAVE_TITLE"
+      );
+
+    userTitle.isEnabled = false;
+
+    return userTitle.save();
+  }
 }
